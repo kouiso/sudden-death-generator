4746da066650a2fe22b88e4d1bb7e190e327e30b
KosukeIsogai <kouiso@ritmo.co.jp>
2024-08-19 03:28:18 +0900

    chore: Update Vuetify configuration and dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  css: ['vuetify/lib/styles/main.sass'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-container>
+          <v-btn @click="showMessage">Click me</v-btn>
+          <v-alert v-if="message" type="success">{{ message }}</v-alert>
+        </v-container>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const message = ref('')
+  const showMessage = () => {
+    message.value = 'Hello World!'
+  }
+  </script>
